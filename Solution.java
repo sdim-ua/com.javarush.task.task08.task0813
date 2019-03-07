11a835cc016cc881db94c945c73430864b9f19c7
sdim-ua <ingersol@yandex.ua>
2019-03-07 09:41:38 +0300

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,28 @@
+import java.util.Set;
+import java.util.HashSet;
+
+/* 
+20 слов на букву «Л»
+*/
+
+public class Solution {
+    public static Set<String> createSet() {
+        //напишите тут ваш код
+        Set<String> list= new Set<String>();
+        list.add("Л1");
+        list.add("Л1");
+        list.add("Л1");
+        list.add("Л1");
+        list.add("Л1");
+        list.add("Л1");
+        list.add("Л1");
+        list.add("Л1");
+        list.add("Л1");
+        list.add("Л1");
+        
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
