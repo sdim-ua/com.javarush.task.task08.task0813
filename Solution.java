bb28f4af57e41dbccf48d7cb4ae375b63d1c80ce
sdim-ua <ingersol@yandex.ua>
2019-03-07 09:45:05 +0300

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -10,16 +10,16 @@ public class Solution {
         //напишите тут ваш код
         Set<String> list= new HashSet<>();
         list.add("Л1");
-        list.add("Л1");
-        list.add("Л1");
-        list.add("Л1");
-        list.add("Л1");
-        list.add("Л1");
-        list.add("Л1");
-        list.add("Л1");
-        list.add("Л1");
-        list.add("Л1");
-        
+        list.add("Л2");
+        list.add("Л3");
+        list.add("Л4");
+        list.add("Л5");
+        list.add("Л6");
+        list.add("Л7");
+        list.add("Л8");
+        list.add("Л9");
+        list.add("Л10");
+      return list  
     }
 
     public static void main(String[] args) {
